c284af462f8b1d602575f526dd7a70c60222a082
incognito <chorobekovali.pi.2.19@gmail.com>
2021-01-26 22:08:18 +0600

    first commit

---
+++ test rashirenie.py
@@ -0,0 +1,80 @@
+#lan = ["go" , "java" , "php" , "python" , "javascript" , "ruby"]
+#l = 0
+#while lan[l] != "php":
+#	print(lan[l])
+#	l += 1
+
+
+
+#a = 7
+#while a <= 7**5 :
+#	print(a)
+#	a *= 7
+
+
+
+#languages = ["go" , "java" , "php" , "python" , "javascript" , "ruby"]
+#for i,languages in enumerate(languages):
+#	print(i + 1, languages)
+
+
+
+#names = ("Ali" , "Ailna" , "Alisher")
+#print(names[0])
+#print(names[1])
+#print(names[2])
+
+
+
+#names = ("Ali" , "Ailna" , "Alisher" , "Alibek" , "Alihan")
+#brathers = []
+#brathers.append("0 , 1 , 2 , 3 , 4")
+#print(brathers)
+
+
+
+#numbers = (1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14 , 15)
+#print(numbers[:3])
+#print(numbers[3:6])
+#print(numbers[6:9])
+#print(numbers[9:12])
+#print(numbers[12:15])
+
+
+#fruits = ["apple", "banana", "cherry"]
+#for x in fruits:
+#  print(x)
+#  if x == "banana":
+#    break
+
+
+#fruits = ["apple", "banana", "cherry"]
+#for x in fruits:
+#	if x == "banana":
+#    break
+#prinе(x)
+
+
+
+#menu =  {"lagman" : 120, "plov"  : 120, "borsh" : 100}
+#print(menu)
+
+
+#program_lang = {
+#	"baha":"java"
+#	"kama":"python"
+#	"alya":"ruby"}
+#user = program_lang["baha"].title()
+#print(f"{program_lang["baha"].title()}"
+#	"f" Favorite lang is {user}")"
+
+
+menu = {
+	"lagman":120,
+	"borsh":150}
+menu["besh_barmak"] =130
+menu.update({"lagman":135})
+menu.pop("borsh")
+menu.update({"coca cola , sprite , fanta"})
+print(menu)
+
